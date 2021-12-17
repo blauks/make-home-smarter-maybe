1b21e64e0216fd637304a28307bd11b2225e0b30
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-12-17 16:12:07 +0100

    Finalize smartplug storage script

--- getSmartPlug.py
+++ getSmartPlug.py
@@ -3,29 +3,70 @@ import json
 import datetime
 import time
 
+# Devices file is generated with python -m tinytuya wizard
 devicesFile = open("devices.json", "r")
 devices = json.load(devicesFile)
 devicesFile.close()
 
 device = tinytuya.OutletDevice(devices[0]["id"], "0.0.0.0", devices[0]["key"])
 device.set_version(3.3)
+device.set_socketPersistent(True)
 
-historicData = []
-historicDataFile = open("historic.json", "w+")
+historicData = {}
+readHistoricDataFile = open("historic.json", "r")
 
-if not historicDataFile.read() == "":
-    historicData = json.loads(historicDataFile)
+try:
+    historicData = json.loads(readHistoricDataFile.read())
+except: pass
 
+payload = device.generate_payload(tinytuya.DP_QUERY)
+data = {}
 
 while True:
-    data = device.status()
-    energyData = {str(datetime.datetime.now()) : {"mA": data['dps']['18'], "W": data['dps']['19']}}
-    historicData.append(energyData)
-    historicDataFile.write(json.dumps(historicData))
+    device.send(payload)
+    response = device.receive()
+
+    if not response == None: 
+        data.update(response['dps'])
+    
+    print(response)
+
+    date = datetime.datetime.now()
+    energyData = {str(date) : {"mA": data['18'], "W": data['19']}}
+    
+    year = date.strftime("%Y")
+    month = date.strftime("%B")
+    day = date.strftime("%d")
+
+    # :|
+    if historicData.get(year) == None:
+        historicData[date.strftime("%Y")] = {}
+
+    # :)
+    if historicData[year].get(month) == None:
+        historicData[year][month] = {"days": {}}
+
+    # :D
+    if historicData[year][month]["days"].get(day) == None:
+        historicData[year][month]["days"][day] = []
+
+    historicData[year][month]["days"][day].append(energyData)
+
+    if not historicData.get(str(int(year)-1), {}).get(month) == None:
+        del historicData[str(int(year)-1)][month]
+        if len(historicData[str(int(year)-1)]) < 1:
+            del historicData[str(int(year)-1)]
+
+    writeHistoricDataFile = open("historic.json", "w")
+    writeHistoricDataFile.write(json.dumps(historicData, indent=4, sort_keys=True))
+    writeHistoricDataFile.close()
 
     print("------- CURRENT DATA -------")
     print("Time: " + str(datetime.datetime.now()))
-    print("Current: " + str(data['dps']['18']))
-    print("Power: " + str(data['dps']['19']))
+    print("Current: " + str(data['18']))
+    print("Power: " + str(data['19']))
     print("----------------------------")
-    time.sleep(5)
+    
+    time.sleep(600)
+
+    payload = device.generate_payload(tinytuya.UPDATEDPS)
